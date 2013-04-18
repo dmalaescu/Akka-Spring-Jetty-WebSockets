a429fce572ac10ad5ac3723892c338cfca0885f6
Dumitru Malaescu <dumitru.malaescu@gmail.com>
2013-04-18 18:35:39 +0300

    first

--- web/src/main/java/com/examples/Bootstrap.java
+++ web/src/main/java/com/examples/Bootstrap.java
@@ -1,10 +1,19 @@
 package com.examples;
 
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.routing.RoundRobinRouter;
+import com.examples.akka.ImageMaster;
+import com.examples.akka.ImageWorker;
 import com.examples.akka.service.DummyService;
 import com.examples.com.examples.akka.service.impl.DummyServiceImpl;
+import com.examples.spring.di.DependencyInjectionProps;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
 
 /**
  * Created with IntelliJ IDEA.
@@ -13,9 +22,34 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ComponentScan("com.example")
 public class Bootstrap {
+    @Autowired
+    ApplicationContext applicationContext;
 
     @Bean
     public DummyService dummyService(){
         return new DummyServiceImpl();
     }
+
+    @Bean(name = "actorSystem")
+    public ActorSystem actorSystem(){
+        return ActorSystem.create("spring-akka-websockets");
+    }
+
+    @Bean
+    @DependsOn("actorSystem")
+    public ActorRef imageMaster(){
+        return actorSystem().actorOf(
+                new DependencyInjectionProps(applicationContext, ImageMaster.class),"imageMaster"
+        );
+    }
+
+    @Bean
+    @DependsOn("actorSystem")
+    public ActorRef imageWorker(){
+        return actorSystem().actorOf(
+                (new DependencyInjectionProps(applicationContext, ImageWorker.class))
+                        .withRouter(new RoundRobinRouter(4)),"imageWorker"
+        );
+    }
+
 }
